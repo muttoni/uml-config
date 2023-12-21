11e2759b8540c45538240250d71dda45cf8a4b84
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2023-12-21 14:30:16 +0100

    fixed padding0

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+    <main className="flex min-h-screen flex-col items-center justify-start p-2 py-10 md:p-10 lg:p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
         </div>
